cfeb95a3f1d097f59dbcb9fd926f913b9973e507
Falesz <jnjwrg@inf.elte.hu>
2024-07-03 17:53:44 +0200

    Game table initialized with empty rows and columns

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 // Main script for Tic-Tac-Toe game
+const gameTable = document.getElementById("gameTable")
 
 function initTable() {
-    console.log("Hello World!");
+    gameTable.innerHTML += "<tr><td></td><td></td><td></td></tr>";
+    gameTable.innerHTML += "<tr><td></td><td></td><td></td></tr>";
+    gameTable.innerHTML += "<tr><td></td><td></td><td></td></tr>";
+
+    // useful stuff:
+    // gameTable.rows.length
+    // gameTable.rows[i]
+    // gameTable.rows.cells.length
+    // gameTable.rows.cells[i]
+
+
 }
 
 initTable();
