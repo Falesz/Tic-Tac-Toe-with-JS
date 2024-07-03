e71858cb08886db3c07977f363bc68329db799f9
Falesz <jnjwrg@inf.elte.hu>
2024-07-03 18:34:18 +0200

    Added logic for players taking turns making their moves

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // Main script for Tic-Tac-Toe game
-const gameTable = document.getElementById("gameTable")
+const gameTable = document.getElementById("gameTable");
+let playerXTurn = true;
 
 function initTable() {
-    gameTable.innerHTML += "<tr><td>X</td><td>X</td><td>X</td></tr>";
-    gameTable.innerHTML += "<tr><td>X</td><td>X</td><td>X</td></tr>";
-    gameTable.innerHTML += "<tr><td>X</td><td>X</td><td>X</td></tr>";
+    gameTable.innerHTML += "<tr><td></td><td></td><td></td></tr>";
+    gameTable.innerHTML += "<tr><td></td><td></td><td></td></tr>";
+    gameTable.innerHTML += "<tr><td></td><td></td><td></td></tr>";
 
     // useful stuff:
     // gameTable.rows.length
@@ -13,4 +14,20 @@ function initTable() {
     // gameTable.rows.cells[i]
 }
 
+function addEventListenersToGameTableCells() {
+    for (i = 0; i < gameTable.rows.length; ++i) {
+        for (j = 0; j < gameTable.rows[i].cells.length; ++j) {
+            gameTable.rows[i].cells[j].addEventListener("click", (event) => {
+                if (event.target.innerText.length != 0) {
+                    return;
+                }
+                
+                event.target.innerText = playerXTurn ? "X" : "O";
+                playerXTurn = !playerXTurn;
+            });
+        }
+    }
+}
+
 initTable();
+addEventListenersToGameTableCells();
